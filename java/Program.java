d1489248b083160da102f619a72ec229b6cebdf6
Lluc Fontcuberta Inacia <lfontcuberta20@ilg.cat>
2022-10-10 20:49:38 +0200

    Revert "deshacer commit"
    This reverts commit ff170df980423deeec62a0adb4e5d1328c6251a3.

--- java/Program.java
+++ java/Program.java
@@ -4,7 +4,7 @@
  * Program to calculate the area and the perimeter
  * of a circle knowing the radius in meter.
  *
- * Copyright 2020 Lluc Fontcuberta <lfontcuberta20i@ilg.cat>
+ * Copyright 2020 Joan Sèculi Lluc Fontcuberta <lfontcuberta20i@ilg.cat>
  *
  * This is free software, licensed under the GNU General Public License v3.
  * See http://www.gnu.org/licenses/gpl.html for more information.
